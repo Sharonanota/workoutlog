e3204a357262784ca6df3a5b803235cc795555df
Sharonanota <matakwamatakwa>
2022-05-26 19:51:51 +0300

    first commit SplashActivity.kt

---
+++ app/src/main/java/com/siara/workoutlog/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.siara.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPassword: TextInputEditText
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilPassword: TextInputLayout
+    lateinit var tvSignUp: TextView
+    lateinit var btnLogin: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        tvSignUp = findViewById(R.id.tvSignUp)
+        btnLogin = findViewById(R.id.btnLogIn)
+
+        tvSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+
+        }
+    }
+    fun validate (){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+
+        if (email.isBlank()){
+            tilEmail.error= "email Required"
+        }
+        if (password.isBlank()){
+            tilPassword.error="email Required"
+        }
+    }
+}
---
+++ app/src/main/java/com/siara/workoutlog/SignUpActivity.kt
@@ -0,0 +1,68 @@
+package com.siara.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class SignUpActivity : AppCompatActivity() {
+
+    lateinit var etFirstName: TextInputEditText
+    lateinit var etPassword: TextInputEditText
+    lateinit var etConfirm: TextInputEditText
+    lateinit var etLastName: TextInputEditText
+    lateinit var etEmail: TextInputEditText
+    lateinit var tilFirstName: TextInputLayout
+    lateinit var tilLastName: TextInputLayout
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilPassword:TextInputLayout
+    lateinit var tilConfirm:TextInputLayout
+    lateinit var btnSignUp: Button
+    lateinit var etLogin: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        etFirstName = findViewById(R.id.etFirstname)
+        etLastName = findViewById(R.id.etLastName)
+        etConfirm = findViewById(R.id.etConfirm)
+        tilFirstName=findViewById(R.id.tilFirstName)
+        tilLastName = findViewById(R.id.tilLastname)
+        tilConfirm = findViewById(R.id.Confirm)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        btnSignUp = findViewById(R.id.btnSignup)
+        etLogin = findViewById(R.id.etLogin)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSignUp.setOnClickListener {
+            validate()
+
+        }
+    }
+    fun validate (){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+        var Confirm = etConfirm.text.toString()
+
+        if (email.isBlank()){
+            tilEmail.error= "email Required"
+        }
+        if (password.isBlank()){
+            tilPassword.error="email Required"
+        }
+        if (Confirm.isBlank()){
+            tilConfirm.error="Confirmation error"
+        }
+    }
+
+}
